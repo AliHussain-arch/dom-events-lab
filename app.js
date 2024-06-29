2e0f106b6e4a9f4c9b42eb56f6d852914d48130f
AliHussain <alihussain08950@gmail.com>
2024-06-29 21:29:44 +0300

    first commit the calculator doesn't support negative numbers

---
+++ app.js
@@ -0,0 +1,89 @@
+/*-------------------------------- Constants --------------------------------*/
+const buttons = document.querySelectorAll('.button');
+const calculator = document.querySelector('#calculator');
+const display = document.querySelector('.display');
+/*-------------------------------- Variables --------------------------------*/
+let num1 = [];
+let num2= [];
+let answer = Number();
+let operation = [];
+let stop_num2_push = 0;
+/*------------------------ Cached Element References ------------------------*/
+
+/*----------------------------- Event Listeners -----------------------------*/
+buttons.forEach((button) => {
+  button.addEventListener('click', (event) => {
+  });
+});
+
+/*-------------------------------- Functions --------------------------------*/
+calculator.addEventListener('click', (event) => {
+ 
+  if(stop_num2_push === 0){
+    if (event.target.classList.contains('number')) {
+      if (operation.length === 0){
+        num1.push(event.target.innerText);
+        display.textContent = num1.join('');
+      }
+      else {
+        num2.push(event.target.innerText);
+        display.textContent = `${num1.join('')} ${operation} ${num2.join('')}`;
+      }
+    }
+  }
+  
+
+
+  if (num1.length > 0 && num2.length === 0){
+    switch (event.target.innerText) {
+      case '/':
+        operation = ['/'];
+        break;
+      case '*':
+        operation = ['*'];
+        break;
+      case '-':
+        operation = ['-'];
+        break;
+      case '+':
+        operation = ['+'];
+        break;
+    }
+    display.textContent = `${num1.join('')} ${operation} `;
+  }
+
+
+  if(event.target.innerText === '='){
+    if (num2.length !== 0){
+      stop_num2_push ++;
+      switch (operation.join('')){
+        case '/':
+          answer = Number(num1.join(''))/Number(num2.join(''));
+          break;
+        case '*':
+          answer = Number(num1.join(''))*Number(num2.join(''));
+          break;
+        case '-':
+          answer = Number(num1.join(''))-Number(num2.join(''));
+          break;
+        case '+':
+          answer = Number(num1.join(''))+Number(num2.join(''));
+          break;
+      }
+      display.textContent = answer;
+    }
+    else{
+      display.textContent = num1.join('');
+    }
+  }
+
+
+  if (event.target.innerText === 'C'){
+    num1 = [];
+    num2= [];
+    operation = [];
+    stop_num2_push = 0;
+    display.textContent = '';
+  }
+  
+});
